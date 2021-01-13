04675b970d32cee13d3703fb2d9a61ec5b8a9c26
pandeysophia <sophiapandey2017@gmail.com>
2021-01-13 16:13:22 -0700

    "List Comprehension"

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -17,10 +17,7 @@ while len(guessed_states) < 50:
     print(answer_state)
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
@@ -33,5 +30,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-
-
